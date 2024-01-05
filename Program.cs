5aa7bce4e23cb9a6d4caf7b5a99a939d79eb9634
Артем <babenkoartem505@gmail.com>
2024-01-05 13:19:09 +0200

    Program

--- Program.cs
+++ Program.cs
@@ -1,48 +1,66 @@
-
+using TasksWebApp;
 using Microsoft.AspNetCore.ResponseCompression;
 using TasksWebApp.Endpoints;
 
-namespace TasksWebApp;
-class Program
+
+var builder = WebApplication.CreateBuilder();
+// Сервіс для взаємнодії з базою даних.
+builder.Services.AddEntityFrameworkSqlite().AddDbContext<ApplicationContext>();
+// Сервіс для стиснення відповіді.
+builder.Services.AddResponseCompression(options =>
 {
-    static void Main()
+    options.EnableForHttps = true;
+    options.Providers.Add<GzipCompressionProvider>();
+    options.Providers.Add<BrotliCompressionProvider>();
+    options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[]
     {
-        var builder = WebApplication.CreateBuilder();
-        builder.Services.AddEntityFrameworkSqlite().AddDbContext<ApplicationContext>();
-        builder.Services.AddResponseCompression(options =>
-        {
-            options.EnableForHttps = true;
-            options.Providers.Add<GzipCompressionProvider>();
-            options.Providers.Add<BrotliCompressionProvider>();
-            options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[]
-            {
-                "text/html",
-                "application/json"
-            });
-        });
-        builder.Services.AddSingleton<TaskTodayCheckerService>();
-        builder.Services.AddAuthorization();
-        builder.Services.AddAuthentication("Cookies").AddCookie(options =>
-        {
-            options.Cookie.HttpOnly = false;
-            options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-            options.Cookie.Name = "AuthenticationCookie";
-        });
-        var app = builder.Build();
+        "text/html",
+        "application/json"
+    });
+});
+// Сервіс для перевірки чи відображати завдання сьогодні сьогодні.
+builder.Services.AddSingleton<TaskTodayCheckerService>();
+// Ауторизація та аутентифікація.
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication("Cookies").AddCookie(options =>
+{
+    options.Cookie.HttpOnly = false;
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+    options.Cookie.Name = "AuthenticationCookie";
+});
+// Swagger services.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// Cors service for testing api in Swager Editor.
+builder.Services.AddCors();
+var app = builder.Build();
 
-        app.UseResponseCompression();
-        app.Services.StartTaskTodayChecker();
-
-        app.UseStaticFiles();
-        app.UseAuthentication();
-        app.UseAuthorization();
-
-        app.SendIndexPage();
-        app.UseTasksEndpoints();
-        app.UseUserEndpoints();
-        app.UseListOfTasksEndpoints();
-
-        app.Run();
-    }
+if (app.Environment.IsDevelopment())
+{
+    app.UseCors(builder =>
+    {
+        builder.WithOrigins("https://editor.swagger.io")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
+    });
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+// Використати сервіс для стиснення відповіді.
+app.UseResponseCompression();
+// Запуск щодобової перевірки завдань.
+app.Services.StartTaskTodayChecker();
+
+app.UseStaticFiles();
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Застосування кінцевих точок.
+app.UseStaticResourceEndpoints();
+app.UseTasksEndpoints();
+app.UseUserEndpoints();
+app.UseListOfTasksEndpoints();
+
+app.Run();
