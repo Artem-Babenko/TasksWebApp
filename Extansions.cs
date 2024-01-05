2446d5e36e66a0141f1803b1004fee4b3c0685ad
Артем <babenkoartem505@gmail.com>
2024-01-05 13:18:55 +0200

    Extansions

--- Extansions.cs
+++ Extansions.cs
@@ -10,15 +10,29 @@ namespace TasksWebApp;
 public static class Extansions
 {
     /// <summary>
-    /// Додає обробник запиту для надіслання головної сторінки.
+    /// Додає обробник запитів для надсилання головної сторінки та надсилання назв фото для її фону.
     /// </summary>
-    /// <param name="app">Екземпляр додатку.</param>
-    public static void SendIndexPage(this WebApplication app)
+    public static void UseStaticResourceEndpoints(this WebApplication app)
     {
         app.MapGet("/", async (HttpContext context, ApplicationContext db) =>
         {
             context.Response.ContentType = "text/html; charset=utf-8";
             await context.Response.SendFileAsync("wwwroot/html/index.html");
+        });
+
+        // Надсилає шляхи фотографій для заднього фону.
+        app.MapGet("/api/backgrounds", (HttpContext context, ApplicationContext db) =>
+        {
+            var photoFolderPath = "wwwroot/photos/"; // або використовуйте абсолютний шлях
+
+            // Отримати список файлів у папці фотографій
+            var photoFiles = Directory
+                .GetFiles(photoFolderPath, "*.jpg")
+                .Select(Path.GetFileName)
+                .ToList();
+
+            // Повернути список шляхів у форматі JSON
+            return Results.Json(photoFiles);
         });
     }
 
